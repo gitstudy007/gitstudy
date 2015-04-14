7f389dd46a23bd939ea1af70b136d4fe1103e522
liumin <liumin2@kingsoft.com>
2015-04-14 16:48:15 +0800

    say hello again

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test classpath");
 		System.out.println("hello git");
+		System.out.println("say hell again");
 	}
 }
