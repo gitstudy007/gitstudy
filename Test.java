29b8afc9c60785a632f77c759d3fa9323e1c73b4
liumin <liumin2@kingsoft.com>
2015-04-14 17:32:20 +0800

    four and five add

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ public class Test {
 		System.out.println("test classpath");
 		System.out.println("hello git");
 		System.out.println("say hell again");
+		System.out.println("fourth add");
+		System.out.println("five add");
 	}
 }
